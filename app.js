4bb638b76388922ef1663dd1bb6912d1ec9cbe1e
Sawako Ishida <sawako.yoriko@gmail.com>
2015-12-22 00:03:56 -0800

    created card obj constructor

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ this.age = age;
 this.logoLink = logoLink;
 this.score = 0;
 
-// - ID
-// - Age
-// - Icon
-// - score
+
 }
 
 //Create a constructor for Cards
-// - Name
-// - Path/Link of Img
-// - front
-// - back
-// - card index num
+var CardsObj = function(cardName, frontCardImg,backCardImg, cardIndex){
+this.cardName = cardName;
+this.frontCardImg = frontCardImg;
+this.backCardImg = backCardImg;
+this.cardIndex = cardIndex;
+
+}
 var newUser1 = new UserAcct('ishida', 20, 'img/cake.jpg')
 console.log(newUser1);
 var storeData1 = JSON.stringify(newUser1);
 localStorage.setItem('userObj',storeData1);
+
+var newCard1 = new CardsObj('');
