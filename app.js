ed586c4d8705f0f91aff2056907b02feada75de9
Sawako Ishida <sawako.yoriko@gmail.com>
2015-12-23 23:19:19 -0800

    frontcards showing when clicked

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var cards = [];
+var cardsShuffle = [];
 console.log(cards);
-var imgElBack;
-var cardContEl;
+//back of the cards
 var cardBacks;
+var imgElBack;
+
+var cardContEl;
+
 //create a constructor for User Acct
 var UserAcct = function(userID,age,logoLink) {
 this.userID = userID;
@@ -21,6 +25,13 @@ this.cardIndex = cardIndex;
 cards.push(this);
 }
 
+CardsObj.prototype.backCardDisplay = function () {
+  cardBacks = document.getElementById('cardList');
+  imgElBack = document.createElement('img');
+  imgElBack.setAttribute('src',this.backCardImg);
+  imgElBack.setAttribute('alt','Back of a card image');
+  cardBacks.appendChild(imgElBack);
+};
 
 //new user acct object
 var newUser1 = new UserAcct('ishida', 20, 'img/cake.jpg')
@@ -29,52 +40,37 @@ var storeData1 = JSON.stringify(newUser1);
 localStorage.setItem('userObj',storeData1);
 
 //new card object
-var newCard1 = new CardsObj('angrySnpy','img/angry.jpeg','img/pattern2.jpeg',1);
+var newCard1 = new CardsObj('angrySnpy','img/angry.jpeg','img/pattern1.jpeg',1);
 var newCard2 = new CardsObj('birthdaySnpy','img/birthday.jpeg','img/pattern2.jpeg',2);
-var newCard3 = new CardsObj('cool','img/cool.jpeg','img/pattern2.jpeg',3);
-var newCard4 = new CardsObj('flower','img/flower.jpeg','img/pattern2.jpeg',4);
-console.log(newCard1);
-console.log(newCard2);
+var newCard3 = new CardsObj('cool','img/cool.jpeg','img/pattern3.jpeg',3);
+var newCard4 = new CardsObj('flower','img/flower.jpeg','img/pattern4.jpeg',4);
 
 
-function backCardDisplay () {
-  cardBacks = document.getElementsByClassName('cards');
-  for(var i = 0; i < cardBacks.length; i++ ){
-  imgElBack = document.createElement('img');
-  imgElBack.setAttribute('src','img/pattern2.jpeg');
-  imgElBack.setAttribute('alt','Back of a card image');
-  cardBacks[i].appendChild(imgElBack);
-  }
-}
+//creating back of the card
 
-function frontCardDisplay () {
-  // cardContEl = document.getElementsByClassName('frontcards');
-  cardBacks = document.getElementsByClassName('cards');
-  for(var i = 0; i < cardBacks.length; i++ ){
-  // cardContEl[i].src = cards[i].frontCardImg;
-  var imgEl = document.createElement('img');
-  imgEl.src = cards[i].frontCardImg;
-  cardBacks[i].appendChild(imgEl);
-  }
-}
-
-backCardDisplay();
+newCard1.backCardDisplay();
+newCard2.backCardDisplay();
+newCard3.backCardDisplay();
+newCard4.backCardDisplay();
 
 
 
-
-
+//Event listener
 var imgPattern = document.getElementById('cardList');
+
 imgPattern.addEventListener('click',function(event){
   event.preventDefault();
-  if(event.target && event.target.nodeName === 'img'){
-    var imgEl = event.target.nodeName('img');
-    console.log(imgEl);
+  var attributeGet = event.target.getAttribute('src');
+  console.log(attributeGet);
+  if(attributeGet === newCard1.backCardImg){
+  event.target.setAttribute('src',newCard1.frontCardImg);
+  } else if(attributeGet === newCard2.backCardImg){
+  event.target.setAttribute('src',newCard2.frontCardImg);
+  } else if(attributeGet === newCard3.backCardImg){
+  event.target.setAttribute('src',newCard3.frontCardImg);
+  }else if(attributeGet === newCard4.backCardImg){
+  event.target.setAttribute('src',newCard4.frontCardImg);
   }
-
-
-  frontCardDisplay();
-
 });
 
 
